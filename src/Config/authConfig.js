9b2b8659137eb8116f190938d6d8fe76d454e7f0
Stanley Castillo <Stanleycastillocanchari@gmail.com>
2026-05-28 17:13:10 -0500

    prueba6

--- src/Config/authConfig.js
+++ src/Config/authConfig.js
@@ -10,6 +10,9 @@ export const msalConfig = {
   },
 };
 
+console.log("CLIENT ID:", import.meta.env.VITE_ENTRA_CLIENT_ID)
+console.log("TENANT ID:", import.meta.env.VITE_ENTRA_TENANT_ID)
+
 export const loginRequest = {
   scopes: ["User.Read"]
 };
