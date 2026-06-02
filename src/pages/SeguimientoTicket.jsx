ca2a23b7b4dbbc35969fbfd7c1d08ebd364b4f42
Stanley Castillo <Stanleycastillocanchari@gmail.com>
2026-06-02 11:57:13 -0500

    cambios ticket17

--- src/pages/SeguimientoTicket.jsx
+++ src/pages/SeguimientoTicket.jsx
@@ -94,7 +94,7 @@ export default function SeguimientoTicket({ onVolver }) {
         >
 
           {/* TITULO */}
-          <h1 className="text-3xl font-bold text-center text-slate-800" style={{ color: "#305da0" }}>
+          <h1 className="text-3xl font-bold text-center" style={{ color: "#305da0" }}>
             
 
             Seguimiento de Ticket
