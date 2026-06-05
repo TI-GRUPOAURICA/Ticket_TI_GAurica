2326e9df8b21fd999e6b1bca495401df2b5322a1
Stanley Castillo <Stanleycastillocanchari@gmail.com>
2026-06-05 10:45:27 -0500

    icono de app9

--- vite.config.js
+++ vite.config.js
@@ -20,19 +20,19 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/pwa-192x192-B.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/pwa-512x512-B.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/pwa-512x512-B.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
