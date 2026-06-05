8d9a50b3dbf2ac7ece2043c3e6996b0ae3b4c653
Stanley Castillo <Stanleycastillocanchari@gmail.com>
2026-06-05 09:18:10 -0500

    icono de app

--- vite.config.js
+++ vite.config.js
@@ -20,10 +20,14 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icono-aurica.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+               src: '/pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
           }
         ]
       }
