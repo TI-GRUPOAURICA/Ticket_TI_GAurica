fd7eb16df52a14717e9c1357c93458fd1e9424e1
Stanley Castillo <Stanleycastillocanchari@gmail.com>
2026-06-05 09:13:30 -0500

    PRUEBA DE PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
+
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
@@ -17,7 +20,7 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icono aurica.svg',
+            src: '/icono-aurica.svg',
             sizes: 'any',
             type: 'image/svg+xml',
             purpose: 'any maskable'
